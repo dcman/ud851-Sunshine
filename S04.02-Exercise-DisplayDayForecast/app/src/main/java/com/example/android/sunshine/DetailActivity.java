348cc34cb9cf8a5d3ce5a050ddaf0d008f03cab9
dcman <justin.johnson.90@my.csun.edu>
2017-01-04 20:21:58 -0800

    Get weather data from intent and dispaly it in new activity

--- S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,17 +1,22 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
     private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
-
+    private TextView mWeatherData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        // TODO (2) Display the weather forecast that was passed from MainActivity
+        mWeatherData = (TextView) findViewById(R.id.detail_tv);
+        if (getIntent().hasExtra(Intent.EXTRA_TEXT)){
+            mWeatherData.setText(getIntent().getStringExtra(Intent.EXTRA_TEXT));
+        }
     }
 }
