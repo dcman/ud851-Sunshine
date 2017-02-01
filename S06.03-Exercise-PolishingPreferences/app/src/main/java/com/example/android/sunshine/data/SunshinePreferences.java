8416d8223b5be73750568d9296e3df9c8730c183
dcman <justin.johnson.90@my.csun.edu>
2017-02-01 15:39:59 -0800

    Done

--- S06.03-Exercise-PolishingPreferences/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
+++ S06.03-Exercise-PolishingPreferences/app/src/main/java/com/example/android/sunshine/data/SunshinePreferences.java
@@ -106,9 +106,10 @@ public class SunshinePreferences {
      * @return true If metric display should be used
      */
     public static boolean isMetric(Context context) {
-        // TODO (2) Return true if the user's preference for units is metric, false otherwise
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        boolean metric = sharedPreferences.getBoolean(context.getString(R.string.pref_units_key), false);
         /** This will be implemented in a future lesson **/
-        return true;
+        return metric;
     }
 
     /**
